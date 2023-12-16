ea18fa2ab779348e4032671e951ae8dfa32cec1f
Becojo <Becojo@users.noreply.github.com>
2023-12-15 19:08:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "local_file" "readme" {
   content = "This is a readme file"
   filename = "README.md"
 }
+
+data "external" "hello" {
+  program = ["bash", "-c", "echo `date` created > hello-data; echo {}"]
+}
